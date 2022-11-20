f810dd1475c633fa16a6f5722a795d6440d17b10
junior <abelsouzacosta@gmail.com>
2022-11-20 14:12:16 -0300

    refactor: change variable names

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,30 @@
 export function calculateRide (segments) {
 	let result = 0;
 	for (const segment of segments) {
-		if (segment.dist != null && segment.dist != undefined && typeof segment.dist === "number" && segment.dist > 0) {
-			if (segment.ds != null && segment.ds != undefined && segment.ds instanceof Date && segment.ds.toString() !== "Invalid Date") {
+		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
+			if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
 	
 				// overnight
 			
-				if (segment.ds.getHours() >= 22 || segment.ds.getHours() <= 6) {
+				if (segment.date.getHours() >= 22 || segment.date.getHours() <= 6) {
 			
 					// not sunday
-					if (segment.ds.getDay() !== 0) {
+					if (segment.date.getDay() !== 0) {
 						
-						result += segment.dist * 3.90;
+						result += segment.distance * 3.90;
 					// sunday
 					} else {
-						result += segment.dist * 5;
+						result += segment.distance * 5;
 	
 					}
 				} else {
 					// sunday
-					if (segment.ds.getDay() === 0) {
+					if (segment.date.getDay() === 0) {
 			
-						result += segment.dist * 2.9;
+						result += segment.distance * 2.9;
 			
 					} else {
-						result += segment.dist * 2.10;
+						result += segment.distance * 2.10;
 					}
 				}
 			} else {
@@ -34,7 +34,7 @@ export function calculateRide (segments) {
 				return -2;
 			}
 		} else {
-			// console.log(dist);
+			// console.log(distance);
 	
 			return -1;
 		}
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -3,43 +3,43 @@ import { calculateRide } from "../src/main";
 describe('Calculate Ride', () => {
   it('Should calculate a ride for a day in the week not overnight', () => {
     expect(calculateRide([
-      { dist: 10, ds: new Date("2021-03-01T10:00:00") }
+      { distance: 10, date: new Date("2021-03-01T10:00:00") }
     ])).toBe(21)
   })
 
   it('Should calculate a ride for a day in the week overnight', () => {
     expect(calculateRide([
-      { dist: 10, ds: new Date("2022-11-21T23:00:00") }
+      { distance: 10, date: new Date("2022-11-21T23:00:00") }
     ])).toBe(39)
   })
 
   it('Should calculate a ride for sunday not overnight', () => {
     expect(calculateRide([
-      { dist: 10, ds: new Date("2022-11-20T10:00:00") }
+      { distance: 10, date: new Date("2022-11-20T10:00:00") }
     ])).toBe(29)
   })
 
   it('should calculate a ride for sunday overnight', () => {
     expect(calculateRide([
-      { dist: 10, ds: new Date("2022-11-20T23:00:00") }
+      { distance: 10, date: new Date("2022-11-20T23:00:00") }
     ])).toBe(50)
   })
 
   it('Should not calculate a ride if the distance is invalid', () => {
     expect(calculateRide([
-      { dist: -10, ds: new Date("2021-03-01T10:00:00") }
+      { distance: -10, date: new Date("2021-03-01T10:00:00") }
     ])).toBe(-1)
   })
 
   it('Should not calculate a ride if the date is invalid', () => {
     expect(calculateRide([
-      { dist: 10, ds: new Date("abcdef") }
+      { distance: 10, date: new Date("abcdef") }
     ])).toBe(-2)
   })
 
   it('Should calculate a ride for the first day of the month', () => {
     expect(calculateRide([
-      { dist: 3, ds: new Date("2021-03-01T10:00:00") }
+      { distance: 3, date: new Date("2021-03-01T10:00:00") }
     ])).toBe(10)
   })
 });
