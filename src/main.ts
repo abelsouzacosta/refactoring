3cfa3e8d060045e409145260f71e1900548a7793
junior <abelsouzacosta@gmail.com>
2022-11-20 14:17:15 -0300

    refactor: remove magic numbers

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,38 @@
 // calculate ride
 export function calculateRide (segments) {
 	let fare = 0;
+	const OVERNIGHT_FARE = 3.90;
+	const OVERNIGHT_SUNDAY_FARE = 5;
+	const SUNDAY_FARE = 2.9;
+	const DAILY_FARE = 2.10;
+	const START_OVERNIGHT = 22;
+	const END_OVERNIGHT = 6;
+	
 	for (const segment of segments) {
 		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
 			if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
 	
 				// overnight
 			
-				if (segment.date.getHours() >= 22 || segment.date.getHours() <= 6) {
+				if (segment.date.getHours() >= START_OVERNIGHT || segment.date.getHours() <= END_OVERNIGHT) {
 			
 					// not sunday
 					if (segment.date.getDay() !== 0) {
 						
-						fare += segment.distance * 3.90;
+						fare += segment.distance * OVERNIGHT_FARE;
 					// sunday
 					} else {
-						fare += segment.distance * 5;
+						fare += segment.distance * OVERNIGHT_SUNDAY_FARE;
 	
 					}
 				} else {
 					// sunday
 					if (segment.date.getDay() === 0) {
 			
-						fare += segment.distance * 2.9;
+						fare += segment.distance * SUNDAY_FARE;
 			
 					} else {
-						fare += segment.distance * 2.10;
+						fare += segment.distance * DAILY_FARE;
 					}
 				}
 			} else {
