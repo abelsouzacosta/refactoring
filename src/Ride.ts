b7a95dc6ee6897e2e722e38a6c555db1638ba59b
Abel Junior <abel.costa@webjump.com.br>
2022-11-23 09:22:55 -0300

    refactor: move responsability to the ride class

--- src/Ride.ts
+++ src/Ride.ts
@@ -3,21 +3,20 @@ import { Segment } from "./Segment";
 export default class Ride {
   segments: Segment[];
   fare: number;
-  OVERNIGHT_FARE = 3.90;
-	OVERNIGHT_SUNDAY_FARE = 5;
-	SUNDAY_FARE = 2.9;
-	DAILY_FARE = 2.10;
-	FIRST_DAY_FARE = 1.5;
-	MINUMUM_FARE = 10;
-
+  OVERNIGHT_FARE = 3.9;
+  OVERNIGHT_SUNDAY_FARE = 5;
+  SUNDAY_FARE = 2.9;
+  DAILY_FARE = 2.1;
+  FIRST_DAY_FARE = 1.5;
+  MINUMUM_FARE = 10;
 
   constructor() {
     this.segments = [];
     this.fare = 0;
   }
 
-  addSegments(segment: Segment) {
-    this.segments.push(segment);
+  addSegments(distance: number, date: Date) {
+    this.segments.push(new Segment(distance, date));
   }
 
   calculateFare() {
@@ -32,17 +31,26 @@ export default class Ride {
         continue;
       }
 
-      if (segment.isOvernight(segment.date) && !segment.isSunday(segment.date)) {
+      if (
+        segment.isOvernight(segment.date) &&
+        !segment.isSunday(segment.date)
+      ) {
         this.fare += segment.distance * this.OVERNIGHT_FARE;
         continue;
       }
 
-      if (!segment.isOvernight(segment.date) && !segment.isSunday(segment.date)) {
+      if (
+        !segment.isOvernight(segment.date) &&
+        !segment.isSunday(segment.date)
+      ) {
         this.fare += segment.distance * this.DAILY_FARE;
         continue;
       }
 
-      if (!segment.isOvernight(segment.date) && segment.isSunday(segment.date)) {
+      if (
+        !segment.isOvernight(segment.date) &&
+        segment.isSunday(segment.date)
+      ) {
         this.fare += segment.distance * this.SUNDAY_FARE;
         continue;
       }
--- test/Ride.spec.ts
+++ test/Ride.spec.ts
@@ -1,62 +1,50 @@
-import Ride from '../src/Ride'
-import { Segment } from '../src/Segment';
+import Ride from "../src/Ride";
+import { Segment } from "../src/Segment";
 
-describe('Ride', () => {
+describe("Ride", () => {
   let ride: Ride;
 
   beforeEach(() => {
     ride = new Ride();
   });
 
-  it('Ride should be implemented', () => {
+  it("Ride should be implemented", () => {
     expect(ride).toBeDefined();
-  })
+  });
 
-  it('Should calculate a ride for the first day in the month', () => {
-    const segment = new Segment(10, new Date("2022-12-01T10:00:00"));
-
-    ride.addSegments(segment);
+  it("Should calculate a ride for the first day in the month", () => {
+    ride.addSegments(10, new Date("2022-12-01T10:00:00"));
 
     expect(ride.calculateFare()).toBe(15);
   });
 
-  it('Should calculate a ride for sunday', () => {
-    const segment = new Segment(10, new Date("2022-12-04T10:00:00"))
-
-    ride.addSegments(segment);
+  it("Should calculate a ride for sunday", () => {
+    ride.addSegments(10, new Date("2022-12-04T10:00:00"));
 
     expect(ride.calculateFare()).toBe(29);
   });
 
-  it('Should calculate a ride for sunday overnight', () => {
-    const segment = new Segment(10, new Date("2022-12-04T23:00:00"));
-
-    ride.addSegments(segment);
+  it("Should calculate a ride for sunday overnight", () => {
+    ride.addSegments(10, new Date("2022-12-04T23:00:00"));
 
     expect(ride.calculateFare()).toBe(50);
-  })
+  });
 
-  it('Should calculate a ride for day in the week overnight', () => {
-    const segment = new Segment(10, new Date("2022-11-21T23:00:00"));
-
-    ride.addSegments(segment);
+  it("Should calculate a ride for day in the week overnight", () => {
+    ride.addSegments(10, new Date("2022-11-21T23:00:00"));
 
     expect(ride.calculateFare()).toBe(39);
-  })
+  });
 
-  it('Should calculate a ride for a day in the week not overnight', () => {
-    const segment = new Segment(10, new Date("2022-11-21T15:00:00"));
-
-    ride.addSegments(segment);
+  it("Should calculate a ride for a day in the week not overnight", () => {
+    ride.addSegments(10, new Date("2022-11-21T15:00:00"));
 
     expect(ride.calculateFare()).toBe(21);
-  })
+  });
 
-  it('Should calculate a ride for the minimum fare', () => {
-    const segment = new Segment(3, new Date("2022-11-21T15:00:00"));
-
-    ride.addSegments(segment);
+  it("Should calculate a ride for the minimum fare", () => {
+    ride.addSegments(3, new Date("2022-11-21T15:00:00"));
 
     expect(ride.calculateFare()).toBe(ride.MINUMUM_FARE);
-  })
-})
+  });
+});
