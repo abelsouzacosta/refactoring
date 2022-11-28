d3c4ed9e725329df8c5f4e45020d28a0ba65c27c
junior <abelsouzacosta@gmail.com>
2022-11-28 02:25:07 -0300

    refactror: adding types

--- src/Ride.ts
+++ src/Ride.ts
@@ -4,20 +4,17 @@ import { Segment } from "./Segment";
 export default class Ride {
   segments: Segment[];
   fare = 0;
-  OVERNIGHT_SUNDAY_FARE = 5;
-  SUNDAY_FARE = 2.9;
-  FIRST_DAY_FARE = 1.5;
   MINUMUM_FARE = 10;
 
   constructor(readonly fareCalculatorHandlers: FareCalculatorHandler) {
     this.segments = [];
   }
 
-  addSegments(distance: number, date: Date) {
+  addSegments(distance: number, date: Date): void {
     this.segments.push(new Segment(distance, date));
   }
 
-  calculateFare() {
+  calculateFare(): number {
     for (const segment of this.segments) {
       this.fare += this.fareCalculatorHandlers.handle(segment);
     }
